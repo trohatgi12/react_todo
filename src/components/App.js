a332a5e108d1e72b12da89735e5b16d247524e1c
Tanay12 <trohatgi@connect.ust.hk>
2020-06-10 10:51:21 +0530

    feat: basic setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./NavBar.js"
+import MainContent from "./MainContent.js"
+import Footer from "./Footer"
+
+
+function App(){
+    return (
+        <div>
+            <NavBar />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
